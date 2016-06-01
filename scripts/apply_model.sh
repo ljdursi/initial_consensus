485b9ebe1a03e478b296fd050c82cde6e6d1da5e
Jonathan Dursi <jonathan@dursi.ca>
2016-06-01 13:55:07 -0400

    Add a cleaning step to remove broad private calls and adjust annotations

--- scripts/apply_model.sh
+++ scripts/apply_model.sh
@@ -5,26 +5,32 @@
 
 module purge 
 module unuse /oicr/local/boutroslab/Modules/modulefiles
+module use /.mounts/labs/simpsonlab/modules/
 module load R/3.2.5
+module load gcc/4.8.1 openblas python/2.7.2 python-packages/2
+module load tabix/0.2.6
 
-readonly DEFTHRESH=0.5
+readonly DEFTHRESH=0.66
 
-if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ ! -f "$1" ] || [ ! -f "$2" ]
+if [ $# -eq 0 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ ! -f "$1" ] || [ ! -f "$2" ]
 then
-    echo "$0 - Apply an ensemble model (provided) to the VCF"
-    echo "Usage: $0 modelfile input.vcf output.vcf [thresh=${DEFTHRESH}]"
+    >&2 echo "$0 - Apply an ensemble model (provided) to the VCF"
+    >&2 echo "Usage: $0 modelfile input.vcf raw_output.vcf output.vcf [thresh=${DEFTHRESH}]"
     exit 
 fi
 
 readonly MODEL=$1
 readonly INPUTVCF=$2
-readonly OUTPUTVCF=$3
-readonly THRESH=${4:-${DEFTHRESH}}
+readonly RAWOUTPUTVCF=$3
+readonly OUTPUTVCF=$4
+readonly THRESH=${5:-${DEFTHRESH}}
 
 nin=$( zcat ${INPUTVCF} | grep -vc "^#" )
 
-Rscript --vanilla scripts/filter_calls_by_model.R $MODEL $INPUTVCF $OUTPUTVCF $THRESH \
-    && mv ${OUTPUTVCF}.bgz ${OUTPUTVCF}.gz \
-    && mv ${OUTPUTVCF}.bgz.tbi ${OUTPUTVCF}.gz.tbi \
+Rscript --vanilla scripts/filter_calls_by_model.R $MODEL $INPUTVCF $RAWOUTPUTVCF $THRESH \
+    && ./scripts/clean_indel_calls.py <( grep -v "##contig" $RAWOUTPUTVCF ) -o $OUTPUTVCF \
+    && bgzip -f ${OUTPUTVCF} \
+    && tabix -p vcf ${OUTPUTVCF}.gz \
+    && nin=$( zcat ${INPUTVCF}.gz | grep -v "^#" | grep -vc "Callers=broad;" ) \
     && nout=$( zcat ${OUTPUTVCF}.gz | grep -vc "^#" ) \
     && if [ $nin -ne $nout ]; then echo "TRUNCATED: ${INPUTVCF} ${OUTPUTVCF}"; fi
---
+++ scripts/clean_indel_calls.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import vcf  
+import sys
+
+def round_dig(value, ndigits=2):
+    scale=10**ndigits
+    return int(num*scale)*1./scale
+
+def main():
+    parser = argparse.ArgumentParser(description='Annotate merged vcf with VAF information where available')
+    parser.add_argument('inputvcf', type=argparse.FileType('r'), default=sys.stdin, help="Merged VCF file")
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help="Specify output file (default:stdout)")
+    args = parser.parse_args()
+
+    reader = vcf.Reader(args.inputvcf)
+    writer = vcf.Writer(args.output, reader)
+
+    for record in reader:
+        new_info = {}
+        # skip broad private calls
+        if record.INFO['Callers'] == ['broad']:
+            continue
+
+        # skip calls that are defined to be SVs
+        varlen = abs(len(record.REF) - len(record.ALT[0]))
+        if varlen >= 100:
+            continue
+
+        # copy some records over directly
+        for item in ['dbsnp', 'cosmic', 'Callers', 'NumCallers', 'repeat_masker', '1000genomes_AF', '1000genomes_ID']:
+            if item in record.INFO:
+                new_info[item] = record.INFO[item]
+
+        if ('TumorVarDepth' in record.INFO) and (record.INFO['TumorVarDepth'] > 0):
+            new_info['t_vaf'] = record.INFO['TumorVAF']
+            new_info['t_alt_count'] = record.INFO['TumorVarDepth']
+            new_info['t_ref_count'] = record.INFO['TumorTotalDepth']-record.INFO['TumorVarDepth']
+
+        record.INFO = new_info
+        writer.write_record(record)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
