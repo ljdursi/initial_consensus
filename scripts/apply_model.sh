c33ad4009844a11d4535441e1a6d2009a0736f1f
Jonathan Dursi <jonathan@dursi.ca>
2016-06-03 12:58:10 -0400

    Reheader filtered models

--- scripts/apply_model.sh
+++ scripts/apply_model.sh
@@ -19,6 +19,8 @@ then
     exit 
 fi
 
+echo "$0 $1 $2 $3 $4 $5"
+
 readonly MODEL=$1
 readonly INPUTVCF=$2
 readonly RAWOUTPUTVCF=$3
@@ -27,8 +29,27 @@ readonly THRESH=${5:-${DEFTHRESH}}
 
 nin=$( zcat ${INPUTVCF} | grep -vc "^#" )
 
+# new header
+cat <<EOF > ${OUTPUTVCF}
+##fileformat=VCFv4.1
+##INFO=<ID=NumCallers,Number=1,Type=Integer,Description="Number of callers that made this call">
+##INFO=<ID=Callers,Number=.,Type=String,Description="Callers that made this call">
+##INFO=<ID=1000genomes_AF,Number=A,Type=Float,Description="Thousand Genomes phase 3 occurance fraction if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.
+20130502.sites.vcf.gz">
+##INFO=<ID=1000genomes_ID,Number=1,Type=String,Description="Thousand Genomes phase 3 ID if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.
+vcf.gz">
+##INFO=<ID=VAF,Number=1,Type=Float,Description="VAF from mutect read filter if available">
+##INFO=<ID=t_alt_count,Number=1,Type=Integer,Description="Tumour alt count from mutect read filter if available">
+##INFO=<ID=t_ref_count,Number=1,Type=Integer,Description="Tumour alt count from mutect read filter if available">
+##INFO=<ID=cosmic,Number=1,Type=String,Description="(first) cosmic ID if found, COSMICv76">
+##INFO=<ID=dbsnp,Number=1,Type=String,Description="(first) dbSNP ID if found, build 147, All_20160408.vcf.gz">
+##INFO=<ID=repeat_masker,Number=1,Type=String,Description="Repeat masker region if in one">
+##FILTER=<ID=LOWSUPPORT,Description="Not called by enough callers in ensemble">
+#CHROM  POS ID  REF ALT QUAL    FILTER  INFO
+EOF
+
 Rscript --vanilla scripts/filter_calls_by_model.R $MODEL $INPUTVCF $RAWOUTPUTVCF $THRESH \
-    && ./scripts/clean_indel_calls.py <( grep -v "##contig" $RAWOUTPUTVCF ) -o $OUTPUTVCF \
+    && ./scripts/clean_indel_calls.py <( grep -v "##contig" $RAWOUTPUTVCF ) | grep -v "^#" >> $OUTPUTVCF \
     && bgzip -f ${OUTPUTVCF} \
     && tabix -p vcf ${OUTPUTVCF}.gz \
     && nin=$( zcat ${INPUTVCF} | grep -v "^#" | grep -vc "Callers=broad;" ) \
