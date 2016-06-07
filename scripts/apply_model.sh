160b5eb70d89cf2a352b93e5a0e26eb1ff397d0f
Jonathan Dursi <jonathan@dursi.ca>
2016-06-07 08:31:42 -0400

    Improve header

--- scripts/apply_model.sh
+++ scripts/apply_model.sh
@@ -34,17 +34,15 @@ cat <<EOF > ${OUTPUTVCF}
 ##fileformat=VCFv4.1
 ##INFO=<ID=NumCallers,Number=1,Type=Integer,Description="Number of callers that made this call">
 ##INFO=<ID=Callers,Number=.,Type=String,Description="Callers that made this call">
-##INFO=<ID=1000genomes_AF,Number=A,Type=Float,Description="Thousand Genomes phase 3 occurance fraction if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.
-20130502.sites.vcf.gz">
-##INFO=<ID=1000genomes_ID,Number=1,Type=String,Description="Thousand Genomes phase 3 ID if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.
-vcf.gz">
+##INFO=<ID=1000genomes_AF,Number=1,Type=Float,Description="Thousand Genomes phase 3 occurance fraction if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz">
+##INFO=<ID=1000genomes_ID,Number=1,Type=String,Description="Thousand Genomes phase 3 ID if found: ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz">
 ##INFO=<ID=VAF,Number=1,Type=Float,Description="VAF from mutect read filter if available">
 ##INFO=<ID=t_alt_count,Number=1,Type=Integer,Description="Tumour alt count from mutect read filter if available">
 ##INFO=<ID=t_ref_count,Number=1,Type=Integer,Description="Tumour alt count from mutect read filter if available">
 ##INFO=<ID=cosmic,Number=1,Type=String,Description="(first) cosmic ID if found, COSMICv76">
 ##INFO=<ID=dbsnp,Number=1,Type=String,Description="(first) dbSNP ID if found, build 147, All_20160408.vcf.gz">
 ##INFO=<ID=repeat_masker,Number=1,Type=String,Description="Repeat masker region if in one">
-##FILTER=<ID=LOWSUPPORT,Description="Not called by enough callers in ensemble">
+##FILTER=<ID=LOWSUPPORT,Description="Not enough support in consensus model">
 #CHROM  POS ID  REF ALT QUAL    FILTER  INFO
 EOF
 
